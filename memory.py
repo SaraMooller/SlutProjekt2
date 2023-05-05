34cf1b618bc9b6061591ccf4d97ac1136352b4ae
Sara Möller <sara.moller05@gmail.com>
2023-05-05 09:58:50 +0200

    skapade kanpparna spm ska bli memoryt

--- memory.py
+++ memory.py
@@ -1,16 +1,27 @@
 import numpy as numpy
 import random
 import os
+import tkinter.font as font
 from tkinter import *
 from tkinter import messagebox
 import threading
 
-class Spelklass(Tk):
-    def __init__(self):
-    self.geometry("600x600")
-    self.config(bg="white")
+class Spelklass():
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("600x600")
+        self.root.config(bg="white")
+        self.font = font.Font(size=30)
+        self.symbols = ["!","$","?"]
+        for column in range(2):
+            for row in range(3):
+                index = random.randint(0,len(self.symbols)-1)
+                symbol = self.symbols[index]
+                button1 = Button(self.root, text=symbol, width=8, height=3, font=self.font)
+                button1.grid(column=column, row=row)
 
-if __name__=="__main__":
-    Spel=Spelklass()
-    Spel.Label()
-    Spel.mainloop
+if __name__ =="__main__":
+    root = Tk()
+    Spel=Spelklass(root)
+    # Spel.Label()
+    root.mainloop()
