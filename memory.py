d63422d707f052333ba39dd7341580e4a1fa2acc
Sara Möller <sara.moller05@gmail.com>
2023-05-05 22:58:02 +0200

    ändradefärg på bakgrunden, knapparna, stl på ruta

--- memory.py
+++ memory.py
@@ -9,15 +9,15 @@ import threading
 class Spelklass():
     def __init__(self, root):
         self.root = root
-        self.root.geometry("600x600")
-        self.root.config(bg="white")
+        self.root.geometry("600x500")
+        self.root.config(bg="black")
         self.font = font.Font(size=30)
         self.symbols = ["!","$","?"]
         for column in range(2):
             for row in range(3):
                 index = random.randint(0,len(self.symbols)-1)
                 symbol = self.symbols[index]
-                button1 = Button(self.root, text=symbol, width=8, height=3, font=self.font)
+                button1 = Button(self.root, text=symbol, width=8, height=3, font=self.font, bg = "pink")
                 button1.grid(column=column, row=row)
 
 if __name__ =="__main__":
